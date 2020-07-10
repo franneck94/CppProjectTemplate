4051e10af1e3b3e010dc28f8fec878690957c0c3
franneck94 <jan2402@DESKTOP-2Q3NQUR.localdomain>
2020-07-10 20:11:09 +0200

    Update

---
+++ cmake/StaticAnalyzer.cmake
@@ -0,0 +1,17 @@
+if(ENABLE_CPPCHECK)
+  if(UNIX)
+    find_program(CMAKE_CXX_CPPCHECK NAMES cppcheck)
+    if (CMAKE_CXX_CPPCHECK)
+        list(
+            APPEND CMAKE_CXX_CPPCHECK 
+                "--enable=warning"
+                "--inconclusive"
+                "--force" 
+                "--inline-suppr"
+                "--suppressions-list=${CMAKE_SOURCE_DIR}/CppCheckSuppressions.txt"
+        )
+    else()
+        message(SEND_ERROR "cppcheck requested but executable not found")
+    endif()
+  endif()
+endif()
