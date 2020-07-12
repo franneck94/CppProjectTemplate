1d631cfabd3b088583de8f0c0724cf1eaa683524
franneck94 <jan2402@DESKTOP-2Q3NQUR.localdomain>
2020-07-12 13:42:23 +0200

    removed doxygen from travis

--- cmake/Documentation.cmake
+++ cmake/Documentation.cmake
@@ -8,7 +8,5 @@ if(DOXYGEN_FOUND)
     configure_file(${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile.in ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile @ONLY)
     add_custom_target(doc 
         ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile &> doxygen.log
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/documentation
-    )
-
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/documentation)
 endif(DOXYGEN_FOUND)
