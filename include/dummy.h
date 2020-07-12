ce5e2dd1cf923c95f7bd65e83f8e7473037e710e
Jan Schaffranek <jan.schaffranek@rub.de>
2020-07-12 19:07:35 +0200

    First commit, got code from cpp template

--- include/dummy.h
+++ include/dummy.h
@@ -1,32 +1,14 @@
-#pragma once
+#ifndef DUMMY_H
+#define DUMMY_H
+
+#include <stdbool.h>
 
 /**
- * @brief This is a dummy class
+ * @brief This is a dummy print function.
  * 
+ * @return true 
+ * @return false 
  */
-class Dummy 
-{
-	public:
+const bool dummyPrint();
 
-  /**
-   * @brief Construct a new Dummy object
-   * 
-   */
-  Dummy();
-  
-  /**
-   * @brief This is a boost dummy function
-   * 
-   * @return true 
-   * @return false 
-   */
-  const bool useBoost();
-
-  /**
-   * @brief This is a linalg dummy function
-   * 
-   * @return true 
-   * @return false 
-   */
-  const bool useLinalg();
-};
+#endif // DUMMY_H
---
+++ tests/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) 
+{
+    printf("UnitTests!\n");
+
+    return 0;
+}
