a7143fea3c7f9e041e255013eb6682d5a0535564
Jan Schaffranek <jan.schaffranek@rub.de>
2020-07-12 20:05:55 +0200

    removed c files

--- include/dummy.h
+++ include/dummy.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #pragma once
 
 /**
@@ -31,19 +30,3 @@ class Dummy
    */
   const bool useLinalg();
 };
-=======
-#ifndef DUMMY_H
-#define DUMMY_H
-
-#include <stdbool.h>
-
-/**
- * @brief This is a dummy print function.
- * 
- * @return true 
- * @return false 
- */
-const bool dummyPrint();
-
-#endif // DUMMY_H
->>>>>>> master
--- tests/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char **argv) 
-{
-    printf("UnitTests!\n");
-
-    return 0;
-}
