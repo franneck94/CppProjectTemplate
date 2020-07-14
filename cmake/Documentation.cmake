150d9394656cc9845f6c149601aad1200cb12d78
franneck94 <jan2402@DESKTOP-2Q3NQUR.localdomain>
2020-07-14 19:19:05 +0200

    Update to 1.0

--- cmake/Documentation.cmake
+++ cmake/Documentation.cmake
@@ -7,6 +7,6 @@ find_package(Doxygen)
 if(DOXYGEN_FOUND)
     configure_file(${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile.in ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile @ONLY)
     add_custom_target(doc 
-        ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile &> doxygen.log
+        ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/documentation)
 endif(DOXYGEN_FOUND)
