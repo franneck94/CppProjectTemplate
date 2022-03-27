36dea71f4bb1485a158c8e1a2493219a3dde5ee3
franneck94 <schaffranek.jan@web.de>
2022-03-27 15:16:44 +0200

    up

--- src/my_lib.cc
+++ src/my_lib.cc
@@ -15,29 +15,3 @@ void cout_hello_world()
 {
     std::cout << "Cout: Hello World" << std::endl;
 }
-
-/**
- * @brief Creates and prints out an instance of the float3 class.
- *
- * @return float3 vector
- */
-linalg::aliases::float3 print_linalg_vector()
-{
-    linalg::aliases::float3 my_float3 { 1, 2, 3 };
-
-    std::cout << "Vec: " << my_float3[0] << ", " << my_float3[1] << ", " << my_float3[2] << std::endl;
-
-    return my_float3;
-}
-
-/**
- * @brief Couts the version of the installed boost library.
- *
- * @return bool
- */
-bool print_boost_version()
-{
-    std::cout << "Boost version: " << BOOST_VERSION << std::endl;
-
-    return true;
-}
--- tests/main.cc
+++ tests/main.cc
@@ -8,19 +8,3 @@ TEST_CASE("Test my cout function", "TestCout")
 {
     cout_hello_world();
 }
-
-TEST_CASE("Test my vector function", "TestVector")
-{
-    linalg::aliases::float3 v = print_linalg_vector();
-
-    REQUIRE(1 == v[0]);
-    REQUIRE(2 == v[1]);
-    REQUIRE(3 == v[2]);
-}
-
-TEST_CASE("Test my boost function", "TestBoost")
-{
-    bool b = print_boost_version();
-
-    REQUIRE(true == b);
-}
