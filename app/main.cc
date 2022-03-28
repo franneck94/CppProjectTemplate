d3a55825b5cecb2c7f4623b7ba79e2bb7684d35c
franneck94 <schaffranek.jan@web.de>
2022-03-28 10:15:30 +0200

    up

--- app/main.cc
+++ app/main.cc
@@ -1,53 +1,65 @@
 #include <iostream>
+#include <fstream>
+#include <filesystem>
 
 #include <cxxopts.hpp>
 #include <fmt/format.h>
 #include <spdlog/spdlog.h>
+#include "nlohmann/json.hpp"
 
 #include "my_lib.h"
 
-auto main(int argc, char **argv) -> int
+using json = nlohmann::json;
+namespace fs = std::filesystem;
+
+constexpr static auto PROJECT_NAME = "CppProjectTemplate";
+
+int main(int argc, char **argv)
 {
     cout_hello_world();
 
-    spdlog::info("Welcome to spdlog!");
+    spdlog::info(fmt::format("Welcome to {}\n", PROJECT_NAME));
 
-    cxxopts::Options options("MyProgram", "One line description of MyProgram");
+    cxxopts::Options options(PROJECT_NAME, "This is all you need to start with C++ projects.");
 
-    options.add_options()
-        ("d,debug", "Enable debugging", cxxopts::value<bool>())
-        ("i,integer", "Int param", cxxopts::value<int>())
-        ("f,file", "File name", cxxopts::value<std::string>())
+    options.add_options("arguments")
+        ("h,help", "Print usage")
+        ("f,filename", "File name", cxxopts::value<std::string>())
         ("v,verbose", "Verbose output", cxxopts::value<bool>()->default_value("false"));
 
     auto result = options.parse(argc, argv);
 
-    if (result.count("debug"))
+    if (result.count("help"))
     {
-        bool debug = result["debug"].as<bool>();
-        std::cout << "debug: " << debug << std::endl;
+        std::cout << options.help() << '\n';
+        return 0;
     }
 
-    if (result.count("integer"))
-    {
-        int integer = result["integer"].as<int>();
-        std::cout << "integer: " << integer << std::endl;
-    }
+    auto filename = std::string{ "C:/Users/Jan/Documents/_LocalCoding/Cpp-Project-Template/app/test.json" };
+    auto verbose = false;
 
     if (result.count("file"))
     {
-        std::string file = result["file"].as<std::string>();
-        std::cout << "file: " << file << std::endl;
+        filename = result["file"].as<std::string>();
     }
 
-    if (result.count("verbose"))
+    verbose = result["verbose"].as<bool>();
+
+    if (verbose)
     {
-        bool verbose = result["verbose"].as<bool>();
-        std::cout << "verbose: " << verbose << std::endl;
+        fmt::print("Opening file: {}\n", filename);
     }
 
-    int a = 42;
-    fmt::print("The answer is {}\n", a);
+    std::ifstream ifs(filename);
+    json parsed_data = json::parse(ifs);
+
+    if (verbose)
+    {
+        const auto name = parsed_data["name"];
+        fmt::print("Name: {}\n", name);
+    }
+
+    int i = 0;
 
     return 0;
 }
