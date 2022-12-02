de5c41902699bbf1a3766b365d8d6dd23ef3c349
franneck94 <schaffranek.jan@web.de>
2022-12-02 08:29:06 +0100

    updated

--- Makefile
+++ Makefile
@@ -16,14 +16,14 @@ install: install_min install_tests
 	sudo apt-get install git llvm pkg-config curl zip unzip tar python3-dev clang-format clang-tidy cppcheck iwyu
 
 install_pip:
-	pip install jinja2 Pygments cmake-format pre-commit conan --user
+	pip install jinja2 Pygments cmake-format pre-commit
 
 install_doc: install_min
 	sudo apt-get install doxygen
 	pip install jinja2 Pygments
 
 setup:
-	pip install conan
+	pip install conan --user
 	conan user
 
 prepare:
--- cmake/Tools.cmake
+++ cmake/Tools.cmake
@@ -1,77 +1,98 @@
+function(add_cmake_format_target)
+    file(GLOB_RECURSE CMAKE_FILES "*/CMakeLists.txt")
+    list(
+        FILTER
+        CMAKE_FILES
+        EXCLUDE
+        REGEX
+        "${CMAKE_SOURCE_DIR}/(build|external)/.*")
+    find_program(CMAKE_FORMAT cmake-format)
+    if(CMAKE_FORMAT)
+        message("---> CMAKE_FORMAT FOUND")
+        message("cmake-format -i -c ${CMAKE_SOURCE_DIR}/.cmake-format.yaml ${CMAKE_FILES}")
+        add_custom_target(
+            run_cmake_format
+            COMMAND cmake-format -i -c ${CMAKE_SOURCE_DIR}/.cmake-format.yaml
+                    ${CMAKE_FILES}
+            WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
+    else()
+        message("---> CMAKE_FORMAT NOT FOUND")
+    endif()
+endfunction()
+
 # iwyu, clang-tidy and cppcheck for certain targets
 function(add_tool_to_target target)
-  get_target_property(TARGET_SOURCES ${target} SOURCES)
-  # Include only cc/cpp files
-  list(FILTER TARGET_SOURCES INCLUDE REGEX \.*\.\(cc|h|cpp|hpp\))
+    get_target_property(TARGET_SOURCES ${target} SOURCES)
+    list(
+        FILTER
+        TARGET_SOURCES
+        INCLUDE
+        REGEX
+        "\.*\.\(cc|h|cpp|hpp\)")
 
-  if(ENABLE_INCLUDE_WHAT_YOU_USE)
-    find_program(INCLUDE_WHAT_YOU_USE include-what-you-use)
-    if(INCLUDE_WHAT_YOU_USE)
-        add_custom_target(${target}_iwyu
-        COMMAND python ${CMAKE_SOURCE_DIR}/tools/iwyu_tool.py
-             -p ${CMAKE_BINARY_DIR}
-             ${TARGET_SOURCES}
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-        )
-    else()
-        message("---> INCLUDE_WHAT_YOU_USE NOT FOUND")
+    if(ENABLE_INCLUDE_WHAT_YOU_USE)
+        find_program(INCLUDE_WHAT_YOU_USE include-what-you-use)
+        if(INCLUDE_WHAT_YOU_USE)
+            add_custom_target(
+                ${target}_iwyu
+                COMMAND python ${CMAKE_SOURCE_DIR}/tools/iwyu_tool.py -p
+                        ${CMAKE_BINARY_DIR} ${TARGET_SOURCES}
+                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+        else()
+            message("---> INCLUDE_WHAT_YOU_USE NOT FOUND")
+        endif()
     endif()
-  endif()
 
-  if(ENABLE_CPPCHECK)
-    find_program(CPPCHECK cppcheck)
-    if(CPPCHECK)
-        message("---> Added Cppcheck for Target: ${target}")
-        add_custom_target(${target}_cppcheck
-        COMMAND ${CPPCHECK}
-            ${TARGET_SOURCES}
-            --enable=all
-            --suppress=unusedFunction,
-            --suppress=unmatchedSuppression,
-            --suppress=missingIncludeSystem,
-            --suppress=toomanyconfigs,
-            --project=${CMAKE_BINARY_DIR}/compile_commands.json
-            -i${CMAKE_BINARY_DIR}/
-            -i${CMAKE_SOURCE_DIR}/external/
-        USES_TERMINAL
-        )
-    else()
-        message("---> CPPCHECK NOT FOUND")
+    if(ENABLE_CPPCHECK)
+        find_program(CPPCHECK cppcheck)
+        if(CPPCHECK)
+            message("---> Added Cppcheck for Target: ${target}")
+            add_custom_target(
+                ${target}_cppcheck
+                COMMAND
+                    ${CPPCHECK} ${TARGET_SOURCES} --enable=all
+                    --suppress=unusedFunction, --suppress=unmatchedSuppression,
+                    --suppress=missingIncludeSystem, --suppress=toomanyconfigs,
+                    --project=${CMAKE_BINARY_DIR}/compile_commands.json
+                    -i${CMAKE_BINARY_DIR}/ -i${CMAKE_SOURCE_DIR}/external/
+                USES_TERMINAL)
+        else()
+            message("---> CPPCHECK NOT FOUND")
+        endif()
     endif()
-  endif()
 
-  if(ENABLE_CLANG_TIDY)
-    find_program(CLANGTIDY clang-tidy)
-    if(CLANGTIDY)
-        message("---> Added Clang Tidy for Target: ${target}")
-        add_custom_target(${target}_clangtidy
-        COMMAND ${CMAKE_SOURCE_DIR}/tools/run-clang-tidy.py
-            ${TARGET_SOURCES}
-            -config-file=${CMAKE_SOURCE_DIR}/.clang-tidy
-            -extra-arg-before=-std=${CMAKE_CXX_STANDARD}
-            -header-filter="\(src|app\)\/*.\(h|hpp\)"
-            -p=${CMAKE_BINARY_DIR}
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-        USES_TERMINAL
-        )
-    else()
-        message("---> CLANGTIDY NOT FOUND")
+    if(ENABLE_CLANG_TIDY)
+        find_program(CLANGTIDY clang-tidy)
+        if(CLANGTIDY)
+            message("---> Added Clang Tidy for Target: ${target}")
+            add_custom_target(
+                ${target}_clangtidy
+                COMMAND
+                    ${CMAKE_SOURCE_DIR}/tools/run-clang-tidy.py
+                    ${TARGET_SOURCES}
+                    -config-file=${CMAKE_SOURCE_DIR}/.clang-tidy
+                    -extra-arg-before=-std=${CMAKE_CXX_STANDARD}
+                    -header-filter="\(src|app\)\/*.\(h|hpp\)"
+                    -p=${CMAKE_BINARY_DIR}
+                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+                USES_TERMINAL)
+        else()
+            message("---> CLANGTIDY NOT FOUND")
+        endif()
     endif()
-  endif()
 
-  if(ENABLE_CLANG_FORMAT)
-    find_program(CLANGFORMAT clang-format)
-    if(CLANGFORMAT)
-    message("---> Added Clang Format for Target: ${target}")
-    add_custom_target(${target}_clangformat
-        COMMAND ${CMAKE_SOURCE_DIR}/tools/run-clang-format.py
-            ${TARGET_SOURCES}
-            --in-place
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-        USES_TERMINAL
-        )
-    else()
-        message("---> CLANGFORMAT NOT FOUND")
+    if(ENABLE_CLANG_FORMAT)
+        find_program(CLANGFORMAT clang-format)
+        if(CLANGFORMAT)
+            message("---> Added Clang Format for Target: ${target}")
+            add_custom_target(
+                ${target}_clangformat
+                COMMAND ${CMAKE_SOURCE_DIR}/tools/run-clang-format.py
+                        ${TARGET_SOURCES} --in-place
+                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+                USES_TERMINAL)
+        else()
+            message("---> CLANGFORMAT NOT FOUND")
+        endif()
     endif()
-  endif()
 endfunction()
