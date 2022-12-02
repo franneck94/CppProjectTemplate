b4e1ed2f36b9a4c6a00f6a18fad86d23cfd7b0a2
franneck94 <schaffranek.jan@web.de>
2022-12-02 09:49:38 +0100

    up

--- tests/main.cc
+++ tests/main.cc
@@ -5,7 +5,7 @@
 
 TEST_CASE("Factorials are computed", "[factorial]")
 {
-    REQUIRE(factorial(0) == 1);
+    REQUIRE(factorial(0)    == 1);
     REQUIRE(factorial(1) == 1);
     REQUIRE(factorial(2) == 2);
     REQUIRE(factorial(3) == 6);
