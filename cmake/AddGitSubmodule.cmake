9a06be2883c7593d61aeabfdcdceeca2b1e9496b
franneck94 <schaffranek.jan@web.de>
2023-05-22 14:14:38 +0200

    small updates

--- cmake/AddGitSubmodule.cmake
+++ cmake/AddGitSubmodule.cmake
@@ -1,16 +1,16 @@
-function(add_git_submodule dir)
+function(add_git_submodule relative_dir)
     find_package(Git REQUIRED)
 
-    if (NOT EXISTS ${CMAKE_SOURCE_DIR}/${dir}/CMakeLists.txt)
+    if (NOT EXISTS ${CMAKE_SOURCE_DIR}/${relative_dir}/CMakeLists.txt)
         execute_process(COMMAND ${GIT_EXECUTABLE}
-            submodule update --init --recursive -- ${CMAKE_SOURCE_DIR}/${dir}
+            submodule update --init --recursive -- ${CMAKE_SOURCE_DIR}/${relative_dir}
             WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
     endif()
 
-    if (EXISTS ${CMAKE_SOURCE_DIR}/${dir}/CMakeLists.txt)
-        message("Adding: ${dir}/CMakeLists.txt")
-        add_subdirectory(${CMAKE_SOURCE_DIR}/${dir})
+    if (EXISTS ${CMAKE_SOURCE_DIR}/${relative_dir}/CMakeLists.txt)
+        message("Adding: ${relative_dir}/CMakeLists.txt")
+        add_subdirectory(${CMAKE_SOURCE_DIR}/${relative_dir})
     else()
-        message("Could not add: ${dir}/CMakeLists.txt")
+        message("Could not add: ${relative_dir}/CMakeLists.txt")
     endif()
 endfunction(add_git_submodule)
